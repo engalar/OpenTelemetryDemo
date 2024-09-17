704151f9af33cd9f97ca011f1d27851c248b5deb
engalar <engalar@gmail.com>
2024-09-17 08:29:44 +0800

    wip

--- mendix-agent/src/main/java/com/wengao/mendix/agent/OpenTelemetryAgent.java
+++ mendix-agent/src/main/java/com/wengao/mendix/agent/OpenTelemetryAgent.java
@@ -1,14 +1,17 @@
 package com.wengao.mendix.agent;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import javassist.NotFoundException;
 
 public class OpenTelemetryAgent {
 
@@ -22,10 +25,10 @@ public class OpenTelemetryAgent {
         var matcherList = new ArrayList<String[]>();
 
         while (matcher.find()) {
-            matcherList.add(new String[]{matcher.group(1), matcher.group(2)});
+            matcherList.add(new String[] { matcher.group(1), matcher.group(2) });
         }
 
-        System.out.println("OpenTelemetry Agent loaded." );
+        System.out.println("OpenTelemetry Agent loaded.");
 
         // 注册字节码转换器
         inst.addTransformer(new ClassFileTransformer() {
@@ -37,38 +40,77 @@ public class OpenTelemetryAgent {
                 var matcherItem = matcherList.stream().filter(matcher -> matcher[0].equals(finalClassName)).findFirst();
                 if (matcherItem.isPresent()) {
                     System.out.println("Transforming class: " + finalClassName);
+                    ClassPool classPool = ClassPool.getDefault();
+                    CtClass ctClass = null;
                     try {
-                        ClassPool classPool = ClassPool.getDefault();
-                        CtClass ctClass = classPool.get(finalClassName);
+                        ctClass = classPool.get(finalClassName);
+                    } catch (NotFoundException e) {
+                        System.out.println("Class not found: " + finalClassName);
+                        e.printStackTrace();
+                    }
 
-                        CtMethod method = ctClass.getDeclaredMethod(matcherItem.get()[1]);
+                    CtMethod method = null;
+                    try {
+                        method = ctClass.getDeclaredMethod(matcherItem.get()[1]);
+                    } catch (NotFoundException e) {
+                        System.out.println("Method not found: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    }
 
+                    try {
                         method.addLocalVariable("span",
                                 classPool.get("io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.Span"));
+                    } catch (CannotCompileException e) {
+                        System.out.println("CannotCompileException: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    } catch (NotFoundException e) {
+                        System.out.println("NotFoundException: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    }
+                    try {
                         method.addLocalVariable("scope",
                                 classPool.get("io.opentelemetry.javaagent.shaded.io.opentelemetry.context.Scope"));
+                    } catch (CannotCompileException e) {
+                        System.out.println("CannotCompileException: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    } catch (NotFoundException e) {
+                        System.out.println("NotFoundException: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    }
 
-                        String code = " " +
-                                "   span = io.opentelemetry.javaagent.shaded.io.opentelemetry.api.GlobalOpenTelemetry.getTracer(\"my-agent-tracer\").spanBuilder(\"span_in_agent\").startSpan();"
-                                +
-                                "scope = span.makeCurrent();"
-                                +
-                                "  try {" +
-                                "      System.out.println(\"OpenTelemetry agent span started\");span.addEvent(\"agent-event\");"
-                                +
-                                "  } catch (Throwable t) {" +
-                                "      span.recordException(t);" +
-                                "      throw t;" +
-                                "  }" +
-                                "";
+                    String code = " " +
+                            "   span = io.opentelemetry.javaagent.shaded.io.opentelemetry.api.GlobalOpenTelemetry.getTracer(\"my-agent-tracer\").spanBuilder(\"span_in_agent\").setParent(io.opentelemetry.javaagent.shaded.io.opentelemetry.context.Context.current()).startSpan();"
+                            +
+                            "scope = span.makeCurrent();"
+                            +
+                            "  try {" +
+                            "      System.out.println(\"OpenTelemetry agent span started\");span.addEvent(\"agent-event\");"
+                            +
+                            "  } catch (Throwable t) {" +
+                            "      span.recordException(t);" +
+                            "      throw t;" +
+                            "  }" +
+                            "";
+                    try {
                         method.insertBefore(
                                 code);
+                    } catch (CannotCompileException e) {
+                        System.out.println("CannotCompileException: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    }
 
+                    try {
                         method.insertAfter(
-                                "scope.close(); span.end(); System.out.println(\"OpenTelemetry span ended\"); ");
+                                "scope.close(); span.end(); System.out.println(\"OpenTelemetry span ended\");");
+                    } catch (CannotCompileException e) {
+                        System.out.println("CannotCompileException: " + matcherItem.get()[1]);
+                        e.printStackTrace();
+                    }
 
+                    try {
                         return ctClass.toBytecode();
-                    } catch (Exception e) {
+                    } catch (IOException | CannotCompileException e) {
+                        System.out.println("CannotCompileException: " + matcherItem.get()[1]);
                         e.printStackTrace();
                     }
                 }
